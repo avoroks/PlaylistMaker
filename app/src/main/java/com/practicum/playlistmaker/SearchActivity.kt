5769b215ec1c0f38bd596507e92f032f34e51cd6
kateskorova <skorova.k@yandex.ru>
2024-04-08 15:22:09 +0300

    дз по спринту 9

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,10 +1,20 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 
 class SearchActivity : AppCompatActivity() {
+    private var textValue = EMPTY_TEXT
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -14,5 +24,49 @@ class SearchActivity : AppCompatActivity() {
         backButton.setOnClickListener {
             this.finish()
         }
+
+        val clearButton = this.findViewById<ImageButton>(R.id.button_clear)
+        val editTextField = this.findViewById<EditText>(R.id.edittext_search)
+        editTextField.setText(textValue)
+
+        clearButton.setOnClickListener {
+            editTextField.setText(EMPTY_TEXT)
+            hideSoftKeyboard(editTextField)
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                textValue = s.toString()
+                clearButton.visibility = receiveButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+        editTextField.addTextChangedListener(textWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, textValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textValue = savedInstanceState.getString(SEARCH_TEXT, EMPTY_TEXT)
+    }
+
+    private fun receiveButtonVisibility(s: CharSequence?) = if (s.isNullOrEmpty()) GONE else VISIBLE
+
+    private fun hideSoftKeyboard(input: EditText) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(input.windowToken, 0);
+    }
+
+    companion object {
+        const val SEARCH_TEXT = "PRODUCT_AMOUNT"
+        const val EMPTY_TEXT = ""
     }
 }
+
